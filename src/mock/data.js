197b68801abf950782dbcb06577f824f70ed4b23
Liudmylla <liudmyla12@yahoo.fr>
2020-11-24 00:09:30 +0100

    create style

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,38 +2,39 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Liudmyla-dev-portfolio', // e.g: 'Name | Developer'
+  lang: 'fr', // e.g: en, es, fr, jp
+  description: 'Front-end developper', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: "Salut, je m'appele",
+  name: 'Liudmylla',
+  subtitle: 'Je suis une développeuse Web ',
+  cta: 'Voir plus',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: 'coming soon',
+  paragraphTwo: 'coming soon',
+  paragraphThree: 'coming soon',
+  // resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: 'https://www.linkedin.com/in/liudmyla-duvivier-05570b15a/', // if no resume, the button will not show up'
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'project1.jpg',
+    title: 'Un site vitrine',
+    info: 'Gatsby+Contentful+Netlify',
+    info2: 'Git+React',
+    url: 'https://ml-facialiste.netlify.app/',
+    repo: 'https://gitlab.com/Liudmylla/marina', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
