9db8b1466278dad9ddb5d35294b534f590f07f57
Calvin Wu <calvinwu4@gmail.com>
2021-01-07 01:00:22 -0500

    Update font-awesome

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,15 @@
 import React, { useContext } from 'react';
 import { Container } from 'react-bootstrap';
 import { Link } from 'react-scroll';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import PortfolioContext from '../../context/context';
 import GithubButtons from '../GithubButtons/GithubButtons';
 
 import { githubButtons } from '../../mock/data';
+
+library.add(fab);
 
 const Footer = () => {
   const { footer } = useContext(PortfolioContext);
@@ -31,7 +36,7 @@ const Footer = () => {
                   target="_blank"
                   aria-label={name}
                 >
-                  <i className={`fa fa-${name || 'refresh'} fa-inverse`} />
+                  <FontAwesomeIcon icon={['fab', `${name}`]} />
                 </a>
               );
             })}
